60ca0cc147fd940188d3cebd12839eacf3956512
UcSe-fps <rehimliazer184@gmail.com>
2026-03-07 14:24:43 +0000

    sixthv2

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for d in range(i + 1, 10):
-        if d > i:
+        if i == 8 and d == 9:
             print("{}{}".format(i, d))
         else:
-            print("{}{}".format(i, d), end="") 
+            print("{}{}, ".format(i, d), end="") 
