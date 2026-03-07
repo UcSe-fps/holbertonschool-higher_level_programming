962be3d398e687f8c56bb440ae09618a603de11f
UcSe-fps <rehimliazer184@gmail.com>
2026-03-07 15:28:55 +0000

    seventh task trial

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    while c in range(97, 123):
-        print("True".format(chr(c)))
+    if ord(c) >= 97 and ord(c) <= 123
+        return True
     else:
-        print("False".format(chr(c)))
-
+        return False
