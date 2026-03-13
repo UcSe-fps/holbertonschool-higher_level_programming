38f715b726cd9e0d8e803aea2b4134dcc9cf784c
UcSe-fps <rehimliazer184@fmail.com>
2026-03-13 11:48:28 +0000

    second what

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 def add(a, b):
-    print("{} + {} = {} ".format(add(add(a + b))))
+    return a + b
 if __name__ == " __main__":
-    import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a + b)))
