1c01aada043f55c4fb41707bd0615c425f92f68c
UcSe-fps <rehimliazer184@gmail.com>
2026-03-14 14:39:17 +0000

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
     max_value = my_list[0]
     for i in my_list:
         if i > max_value:
-                max_value = i
+            max_value = i
     return max_value
