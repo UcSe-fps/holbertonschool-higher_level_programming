28245ea80849bd4f74028c4d9297bac623fd2efb
UcSe-fps <rehimliazer184@gmail.com>
2026-03-14 14:06:48 +0000

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a_1 = 0
     if len(tuple_b):
         b_1 = tuple_b[0]
+    else:
+        b_1 = 0
     if len(tuple_a):
         a_2 = tuple_a[1]
     else:
