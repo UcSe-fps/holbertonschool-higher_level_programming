8711c69d3e3907b410c6d6ec12dcb69c8dfa8e83
UcSe-fps <rehimliazer184@gmail.com>
2026-03-22 10:19:45 +0000

    tenth

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    biggest_key = []
+    for key in biggest_key.keys():
+        if key >= a_dictionary.keys():
+            return biggest_key
+        else:
+            return None
