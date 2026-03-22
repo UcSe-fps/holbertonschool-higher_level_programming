ee934bba3a6521ce50609788b26b26408742a0e0
UcSe-fps <rehimliazer184@gmail.com>
2026-03-22 09:38:59 +0000

    ninth

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = []
-    for i in a_dictionary.keys:
-        new_dictionary = i * 2
+    new_dictionary = a_dictionary
+    for key in new_dictionary:
+        new_dictionary[key] = a_dictionary[key] * 2
     return new_dictionary    
         
