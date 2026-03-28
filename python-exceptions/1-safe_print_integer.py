be7d8d54c41e0a343ccb5d9ff2574d6ce5858668
UcSe-fps <rehimliazer184@gmail.com>
2026-03-28 19:01:55 +0000

    Lucifer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-       print("{:d}".format(value))
+        print("{:d}".format(value))
     except (ValueError, TypeError):
-       return False
-    else:
-       return True
+        return False
+    else:return True
