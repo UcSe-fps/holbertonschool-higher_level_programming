217c21e8aac7f6685bd04b4c6eca6b0e5f16eb1d
UcSe-fps <rehimliazer184@gmail.com>
2026-03-28 19:10:03 +0000

    Michael

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{}".format(my_list), end="")
             count += 1
-    except (ValueError, TypeError):
-        pass
+        except (ValueError, TypeError):
+            pass
     print()
     return count
