72b2f09418e17bde82a1a5e55caa42994abbac03
UcSe-fps <rehimliazer184@gmail.com>
2026-03-28 18:49:48 +0000

    Azazel

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    i = 0
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list=[i]), end="")
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
             count += 1
-        except (IndexError, TypeError):
-        continue
+    except (IndexError, TypeError):
+        pass
     print()
     return count
