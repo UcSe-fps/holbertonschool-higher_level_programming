6364c88203bf442773d67efa6cfeac6de4a787b3
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 13:44:49 +0000

    trying

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a trial
+"""
+
+def is_same_class(obj, a_class):
+   """this is another one"""
+    
+
+    if obj isinstance(a_class_):
+        return True
+    else:
+        return False
