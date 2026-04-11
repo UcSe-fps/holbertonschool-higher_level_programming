80a91a17f1edfce3f513d4cc4cc218142ffc4a99
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 12:47:40 +0000

    zero

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class NewClass:
+    class NewClass:
+    def __init__(self):
+        self.name = "test"
+
+# Create an instance
+    my_inst = NewClass()
+
+# Call the lookup function
+    print(lookup(my_inst))
