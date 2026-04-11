0bed9debed547ae965d6364201e3e2e74651d10e
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 12:52:08 +0000

    zero

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-class NewClass:
-    class NewClass:
-    def __init__(self):
-        self.name = "test"
-
-# Create an instance
-    my_inst = NewClass()
-
-# Call the lookup function
-    print(lookup(my_inst))
+def lookup(object):
+    return dir(obj)
