c0b4b5883d7b369d8ad12d16ea1b74cbb9730a49
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 14:02:06 +0000

    trying

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-
 """this is a module"""
-
 
 
 def is_same_class(obj, a_class):
 
-
     """This will return True or False"""
-
 
     if type(obj) is a_class:
         return True
     else:
-        return False
+         return False
