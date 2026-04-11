eac791219268bd43b1530ae0aee27c82a3842f66
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 13:48:24 +0000

    trying

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,10 +3,10 @@
 """
 
 def is_same_class(obj, a_class):
-   """this is another one"""
+   """this is another True one"""
     
 
-    if obj isinstance(a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
