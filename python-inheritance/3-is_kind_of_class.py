cfce02d18c97e3dc3823cee7f2771a81db91dc3b
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 14:08:18 +0000

    third

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This is a python task"""
+
+def is_kind_of_class(obj, a_class):
+    
+    """true or false"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
