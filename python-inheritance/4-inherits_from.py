759163911e7f8c7d694fe7c849147e0029a23c7d
UcSe-fps <rehimliazer184@gmail.com>
2026-04-11 14:13:51 +0000

    trying

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that inherits"""
+
+
+def inherits_from(obj, a_class):
+    """Function that inherits"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
