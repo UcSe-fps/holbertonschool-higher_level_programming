6d9c1310c2fbda541e92f8d6a467752a37895cf2
UcSe-fps <rehimliazer184@gmail.com>
2026-04-12 12:17:18 +0000

    sixth

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class BaseGeometry:
+"""tihohv"""
+
+
+
+def area(self):
+    """hfbvidbcin"""
+
+    raise Exception("area() is not implemented")
+
+
+"""third definition"""
+
+def integer_validator(self, name, value):
+    self.value = value
+    self.name = name
+
+    """last part"""
+
+    if type(self.value) is not int:
+        raise TypeError("<name> must be an integer")
+    elif value <= 0:
+        raise ValueError("<name> must be greater than 0")
