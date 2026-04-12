559d05ff71780cab012e2cd08115a9b1fcc786ac
UcSe-fps <rehimliazer184@gmail.com>
2026-04-12 13:56:07 +0000

    dkfi

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
+"""Square"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-""""new way"""
-Rectangle = __import__(9-rectangle.py).Rectangle
-
-"""new class"""
 
 class Square(Rectangle):
+    """Square data that inherits from Rectangle"""
+
     def __init__(self, size):
-        self.integer_validator('size', size)
+        """Square"""
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-    
-    """new def"""
 
     def area(self):
-         """returns soze"""
-        
+        """Method to calculate area of the square"""
+
         return self.__size * self.__size
