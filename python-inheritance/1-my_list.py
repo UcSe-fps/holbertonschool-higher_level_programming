638cf618f704ebff869f95c9243409f3bf0f41d2
UcSe-fps <rehimliazer184@gmail.com>
2026-04-18 18:29:06 +0000

    another one [

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
+"""
+    module inherit list
     """
 
+
+class MyList(list):
+    """
+        MyList
+    """
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """
+        Print a sorted List
+        """
+        print(sorted(self))
