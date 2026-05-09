718033c33f8d4f3c2ec8400e53394367f3efabc2
UcSe-fps <rehimliazer184@gmail.com>
2026-05-09 16:44:31 +0000

    rial

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""dfdfdfdfdf"""
+
+
+class Student:
+    """thththth"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """ryryryry"""
+    def to_json(self):
+
+        """ururururur"""
+        return self.__dict__
+
+
