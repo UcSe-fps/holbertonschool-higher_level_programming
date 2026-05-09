af02a4a0aadf477e7078ecb64e345ce87e4c4c22
UcSe-fps <rehimliazer184@gmail.com>
2026-05-09 16:26:54 +0000

    trial and

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""jnjnjjnjjn"""
+import json
+
+
+def load_from_json_file(filename):
+    """hnhnhnhnhnhn"""
+
+    with open(filename, mode='r', encoding='utc-8') as f:
+        return json.loads(f)
