b4b869cecdb4d94653dde6dc2f2721451a1ccb11
UcSe-fps <rehimliazer184@gmail.com>
2026-05-09 15:50:45 +0000

    in.allah

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Reads a UTF8 text file and prints it to stdout."""
 def read_file(filename=""):
-    with open('filename', encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
             print(f.read(), end='')
