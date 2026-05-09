4cc75b19d777c6905328b9130c5184c5141a1ade
UcSe-fps <rehimliazer184@gmail.com>
2026-05-09 16:02:37 +0000

    smth

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""This is something idk"""
+
+def write_file(filename="", text=""):
+
+    """this is also something"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
