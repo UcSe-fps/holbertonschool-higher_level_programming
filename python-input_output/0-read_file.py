d59e06ae4a50e3fb151cdae774978c40c40b3f1b
UcSe-fps <rehimliazer184@gmail.com>
2026-05-09 15:42:09 +0000

    First of the week

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#this is a string
+
+def read_file(filename=""):
+    with open('filename', encoding="utf-8") as read_file:
+        for line in read-file:
+            print(line, end='')
