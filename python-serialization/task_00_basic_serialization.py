a743895349636affee38c8323f0120778336f18e
UcSe-fps <rehimliazer184@gmail.com>
2026-05-15 15:20:08 +0200

    hshs

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,11 +8,12 @@ def serialize_and_save_to_file(data, filename):
 
     with open(filename, 'w', encoding='utf-8') as f:
         return json.dump(data, f)
-    pass
+
+"""this is another one"""
+
 
 def load_and_deserialize(filename):
     """this is too"""
 
     with open(filename, 'w', encoding='utf-8') as f:
         return json.load(f)
-    pass
