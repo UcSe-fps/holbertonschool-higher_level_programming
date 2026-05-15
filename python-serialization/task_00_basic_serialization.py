0f9b9ea7ff6ebcb7a2732331ceb2b148adc02533
UcSe-fps <rehimliazer184@gmail.com>
2026-05-15 15:26:12 +0200

    hshs

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""this is it"""
+"""
+this is a project
+"""
 import json
-
 
 def serialize_and_save_to_file(data, filename):
     """
-    
-    this is another
+    Serializes a Python dictionary and saves it to a JSON file.
 
-
-
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The name of the output JSON file.
     """
-
     with open(filename, 'w', encoding='utf-8') as f:
-         json.dump(data, f)
-
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
     """
+    Loads a JSON file and deserializes it into a Python dictionary.
 
-    this is too
+    Args:
+        filename (str): The name of the JSON file to read.
 
-
+    Returns:
+        dict: The reconstructed Python dictionary.
     """
-
-
-
-
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
