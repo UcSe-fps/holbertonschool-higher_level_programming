1fa7b8bd7fb0cf26ea157343bec3d13376c1a273
UcSe-fps <rehimliazer184@gmail.com>
2026-05-16 16:55:54 +0000

    ikel

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+ -- this is for the 6th task
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT NOT NULL PK AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
