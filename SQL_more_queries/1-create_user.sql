4164554f6d4f7fae383ffab27ea5d1f63897a2f4
UcSe-fps <rehimliazer184@gmail.com>
2026-05-16 16:32:18 +0000

    for me second

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- I will create someone
-CREATE USER IF NOT EXISTS user_0d_1;
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT SELECT
 ON *.*
-TO user_0d_1;
+TO user_0d_1@localhost;
