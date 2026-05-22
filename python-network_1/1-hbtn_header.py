7d0af0f011808eb54694d27394c14aad3aa6829a
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 13:51:33 +0000

    work

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ import sys
 import urllib.request
 
 
-if __name__ =+ '__main__':
+if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1]) as smt:
         print("{}".format(smt.getheader('X-Request-Id')))
