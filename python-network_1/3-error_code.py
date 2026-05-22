d668930e3baad922ee53c2861dad9e698d13aa7f
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 14:20:56 +0000

    really

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         with urllib.request.urlopen(request) as response:
             response.read().decode('utf-8')
     except urllib.error.HTTPError as error:
-        print("Error code: ".format(error.code)
+        print("Error code: {}".format(error.code)
