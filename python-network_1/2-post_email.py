11c1b8d92b25b4a6770cba26c55eacf6826184f0
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 14:10:30 +0000

    lalal

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+"""this should do it"""
+
 import sys
 import urllib.request
 import urllib.parse
-"""somewhot"""
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
     email_value = {'email':sys.argv[2]}
     data = urllib.parse.urlencode(email_value)
-    data = deta.encode('utf-8')
+    data = data.encode('utf-8')
     request = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(request) as response:
-        print("{}".format(response.read().decode('utf-8')))
+        print(response.read().decode('utf-8'))
