8551e9d5304e61173bc4c8e0c7c7d8154c1ec226
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 16:20:15 +0000

    something

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     url = ("https://api.github.com/user")
     username = sys.argv[1]
     password = sys.argv[2]
-    infro = (username, password)
+    info = (username, password)
     r = requests.get(url, auth=info)
     try:
         print(r.json()['id'])
