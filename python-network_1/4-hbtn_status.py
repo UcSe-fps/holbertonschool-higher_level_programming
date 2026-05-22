11979f4e3fca224428a770d98bb6d816a640ccf4
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 14:32:39 +0000

    lad

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this will be smt"""
+import requests
+
+if __name__ == '__main__':
+    this = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
