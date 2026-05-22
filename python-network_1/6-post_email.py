2b9e41cb40e52c24224858715300d5d5592f76b8
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 15:18:16 +0000

    right

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == '__main__':
     para = {'email' : sys.argv[2]}
-    r = requests.get(sys.argv[1], para)
+    r = requests.post(sys.argv[1], data=para)
     print(r.text)
