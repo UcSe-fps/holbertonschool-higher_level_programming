2f43ae1d5a339dd068ac4426590f1fd9694a4641
UcSe-fps <rehimliazer184@gmail.com>
2026-05-22 14:20:00 +0000

    really

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this will do it"""
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    request = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            response.read().decode('utf-8')
+    except urllib.error.HTTPError as error:
+        print("Error code: ".format(error.code)
